2ed382a89a455abd33edff32b6612bc84adb4a3a
Kilian Valkhof <kilian@kilianvalkhof.com>
2021-03-25 15:19:44 +0100

    project

---
+++ src/preload.js
@@ -0,0 +1,9 @@
+const { ipcRenderer, contextBridge } = require("electron");
+
+contextBridge.exposeInMainWorld(
+  'scratchpad',
+  {
+    saveContent: (content) => ipcRenderer.send('saveContent', content),
+    content: ipcRenderer.invoke("loadContent"),
+  }
+)
---
+++ src/renderer.js
@@ -0,0 +1,4 @@
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import './app.jsx';
+import './index.css';
