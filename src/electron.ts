3d36753656a6099f53d813938c26597f9120d803
Cary Meskell <carymeskell@gmail.com>
2023-07-21 00:45:41 -0700

    get a shell of a react page going with fields

--- src/electron.ts
+++ src/electron.ts
@@ -1,18 +1,30 @@
-// src/electron.js
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
-function createWindow() {
-  // Create the browser window.
-  let win = new BrowserWindow({
+function createWindow () {
+  const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
-    },
-  });
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
 
-  // and load the index.html of the app.
-  win.loadFile('index.html');
+  win.loadFile('index.html')
 }
 
-app.on('ready', createWindow);
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
--- src/react.tsx
+++ src/react.tsx
@@ -2,8 +2,81 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-const Index = () => {
-  return <div>Hello React!</div>;
+import { useState } from 'react';
+
+interface Props {
+  label: string;
+  value: string;
+  setValue: (value: string) => void
+}
+
+const InputField = ({ label, value, setValue }: Props) => {
+  function handleValueChange(
+    e: React.ChangeEvent<HTMLInputElement>,
+    setFn: (value: string) => void
+  ) {
+    setFn(e.target.value);
+  }
+
+  return (
+    <div className="input-field layout-column">
+      <label>{label}</label>
+      <input value={value} onChange={(e) => handleValueChange(e, setValue)} />
+    </div>
+  );
 };
 
-ReactDOM.render(<Index />, document.getElementById('app'));
+const App = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [location, setLocation] = useState('');
+  const [conditionCL, setConditionCL] = useState('');
+  const [conditionFB, setConditionFB] = useState('');
+  const [manufacturer, setManufacturer] = useState('');
+  const [name, setName] = useState('');
+  const [neighborhood, setNeighborhood] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [zipCode, setZipCode] = useState('');
+  const [isHiddenFromFriends, setHideFromFriends] = useState(true);
+
+  function handleHideFromFriends(e: React.ChangeEvent<HTMLInputElement>) {
+    setHideFromFriends(e.target.checked);
+  }
+
+  return (
+    <div>
+      <h1 className="text-align-center">Relister</h1>
+      <h2 className="text-align-center">Create New Posting</h2>
+      <h3>Site Agnostic Fields</h3>
+      
+      <InputField label="Title" value={title} setValue={setTitle} />
+      <InputField label="Description" value={description} setValue={setDescription} />
+      <InputField label="Price" value={price} setValue={setPrice} />
+      <InputField label="Location" value={location} setValue={setLocation} />
+
+      {/* FB */}
+      <h3>Facebook Marketplace-specific Fields</h3>
+
+      <InputField label="Condition" value={conditionFB} setValue={setConditionFB} />
+      <label>Hide From Friends</label>
+      <input
+        type="checkbox"
+        checked={isHiddenFromFriends}
+        onChange={handleHideFromFriends}
+      />
+
+      {/* CL */}
+      <h3>Craigslist-specific Fields</h3>
+
+      <InputField label="Condition" value={conditionCL} setValue={setConditionCL} />
+      <InputField label="Manufacturer" value={manufacturer} setValue={setManufacturer} />
+      <InputField label="Name" value={name} setValue={setName} />
+      <InputField label="Neighborhood" value={neighborhood} setValue={setNeighborhood} />
+      <InputField label="PhoneNumber" value={phoneNumber} setValue={setPhoneNumber} />
+      <InputField label="ZipCode" value={zipCode} setValue={setZipCode} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
