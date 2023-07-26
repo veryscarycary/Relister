b1cf287ff20da300942b195321d455a52508a320
Cary Meskell <carymeskell@gmail.com>
2023-07-26 15:13:11 -0700

    fix tabbing

--- src/app.jsx
+++ src/app.jsx
@@ -95,6 +95,9 @@ const App = () => {
     setHideFromFriends(e.target.checked);
   }
 
+  console.log('selectedTab');
+  console.log(selectedTab);
+
   const createNewPosting = async () => {
     await window.scratchpad.createNewPosting({
       title,
@@ -118,159 +121,179 @@ const App = () => {
         <h1 className="mt-24 mb-24">Relister</h1>
 
         <div className="pc-tab">
-          <input id="tab1" type="radio" name="pct" checked={selectedTab === 'create'} onChange={() => setSelectedTab('create')} />
-          <input id="tab2" type="radio" name="pct" checked={selectedTab === 'relist'} onChange={() => setSelectedTab('relist')} />
+          <input
+            className="tab-input"
+            id="tab1"
+            type="radio"
+            name="pct"
+            checked={selectedTab === 'create'}
+            onChange={() => setSelectedTab('create')}
+          />
+          <input
+            className="tab-input"
+            id="tab2"
+            type="radio"
+            name="pct"
+            checked={selectedTab === 'relist'}
+            onChange={() => setSelectedTab('relist')}
+          />
           <nav>
             <ul>
-              <li className="tab1">
+              <li
+                className={`tab-li ${
+                  selectedTab === 'create' ? 'selected' : ''
+                }`}
+              >
                 <label for="tab1">Create New Posting</label>
               </li>
-              <li className="tab2">
+              <li
+                className={`tab-li ${
+                  selectedTab === 'relist' ? 'selected' : ''
+                }`}
+              >
                 <label for="tab2">Relist</label>
               </li>
             </ul>
           </nav>
           <section>
-            <div className="form-section tab1">
-              <SegmentedControl
-                className="mb-12"
-                mode={selectedApp}
-                setMode={setSelectedApp}
-              />
+            {selectedTab === 'create' && (
+              <div className="form-section tab-section">
+                <SegmentedControl
+                  className="mb-12"
+                  mode={selectedApp}
+                  setMode={setSelectedApp}
+                />
 
-              <div className="form-section flex-50 mb-16">
-                <h3 className="field-header mb-16">General Fields</h3>
+                <div className="form-section flex-50 mb-16">
+                  <h3 className="field-header mb-16">General Fields</h3>
 
-                <InputField
-                  className="mb-8"
-                  label="Title"
-                  value={title}
-                  setValue={setTitle}
-                  required
-                />
-                <TextareaField
-                  className="mb-8"
-                  label="Description"
-                  value={description}
-                  setValue={setDescription}
-                  required
-                />
-                <InputField
-                  className="mb-8"
-                  label="Price"
-                  value={price}
-                  setValue={setPrice}
-                  required
-                />
-                <InputField
-                  className="mb-8"
-                  label="Location"
-                  value={location}
-                  setValue={setLocation}
-                  required
-                />
+                  <InputField
+                    className="mb-8"
+                    label="Title"
+                    value={title}
+                    setValue={setTitle}
+                    required
+                  />
+                  <TextareaField
+                    className="mb-8"
+                    label="Description"
+                    value={description}
+                    setValue={setDescription}
+                    required
+                  />
+                  <InputField
+                    className="mb-8"
+                    label="Price"
+                    value={price}
+                    setValue={setPrice}
+                    required
+                  />
+                  <InputField
+                    className="mb-8"
+                    label="Location"
+                    value={location}
+                    setValue={setLocation}
+                    required
+                  />
+                </div>
+
+                <div id="specific-fields" className="layout-row mb-16">
+                  {/* FB */}
+                  {(selectedApp === 'both' || selectedApp === 'fbm') && (
+                    <div className="form-section flex-50">
+                      <h3 className="field-header mb-16">
+                        FB Marketplace Fields
+                      </h3>
+
+                      <InputField
+                        className="mb-8"
+                        label="Condition"
+                        value={conditionFB}
+                        setValue={setConditionFB}
+                        required
+                      />
+                      <label className="mr-8">Hide From Friends</label>
+                      <input
+                        type="checkbox"
+                        checked={isHiddenFromFriends}
+                        onChange={handleHideFromFriends}
+                        required
+                      />
+                    </div>
+                  )}
+
+                  {/* CL */}
+                  {(selectedApp === 'both' || selectedApp === 'cl') && (
+                    <div className="form-section border-left-0 flex-50">
+                      <h3 className="field-header mb-16">CL Fields</h3>
+
+                      <InputField
+                        className="mb-8"
+                        label="Condition"
+                        value={conditionCL}
+                        setValue={setConditionCL}
+                      />
+                      <InputField
+                        className="mb-8"
+                        label="Manufacturer"
+                        value={manufacturer}
+                        setValue={setManufacturer}
+                      />
+                      <InputField
+                        className="mb-8"
+                        label="Name"
+                        value={name}
+                        setValue={setName}
+                        required
+                      />
+                      <InputField
+                        className="mb-8"
+                        label="Neighborhood"
+                        value={neighborhood}
+                        setValue={setNeighborhood}
+                        required
+                      />
+                      <InputField
+                        className="mb-8"
+                        label="PhoneNumber"
+                        value={phoneNumber}
+                        setValue={setPhoneNumber}
+                        required
+                      />
+                      <InputField
+                        className="mb-8"
+                        label="ZipCode"
+                        value={zipCode}
+                        setValue={setZipCode}
+                        required
+                      />
+                    </div>
+                  )}
+                </div>
+
+                <div className="layout-row layout-align-end">
+                  <button className="button-primary" onClick={createNewPosting}>
+                    Create
+                  </button>
+                </div>
               </div>
-
-              <div id="specific-fields" className="layout-row mb-16">
-                {/* FB */}
-                {(selectedApp === 'both' || selectedApp === 'fbm') && (
-                  <div className="form-section flex-50">
-                    <h3 className="field-header mb-16">
-                      FB Marketplace Fields
-                    </h3>
-
-                    <InputField
-                      className="mb-8"
-                      label="Condition"
-                      value={conditionFB}
-                      setValue={setConditionFB}
-                      required
-                    />
-                    <label className="mr-8">Hide From Friends</label>
-                    <input
-                      type="checkbox"
-                      checked={isHiddenFromFriends}
-                      onChange={handleHideFromFriends}
-                      required
-                    />
-                  </div>
-                )}
-
-                {/* CL */}
-                {(selectedApp === 'both' || selectedApp === 'cl') && (
-                  <div className="form-section border-left-0 flex-50">
-                    <h3 className="field-header mb-16">CL Fields</h3>
-
-                    <InputField
-                      className="mb-8"
-                      label="Condition"
-                      value={conditionCL}
-                      setValue={setConditionCL}
-                    />
-                    <InputField
-                      className="mb-8"
-                      label="Manufacturer"
-                      value={manufacturer}
-                      setValue={setManufacturer}
-                    />
-                    <InputField
-                      className="mb-8"
-                      label="Name"
-                      value={name}
-                      setValue={setName}
-                      required
-                    />
-                    <InputField
-                      className="mb-8"
-                      label="Neighborhood"
-                      value={neighborhood}
-                      setValue={setNeighborhood}
-                      required
-                    />
-                    <InputField
-                      className="mb-8"
-                      label="PhoneNumber"
-                      value={phoneNumber}
-                      setValue={setPhoneNumber}
-                      required
-                    />
-                    <InputField
-                      className="mb-8"
-                      label="ZipCode"
-                      value={zipCode}
-                      setValue={setZipCode}
-                      required
-                    />
-                  </div>
-                )}
+            )}
+            {selectedTab === 'relist' && (
+              <div className="form-section tab-section">
+                <h2>Second</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Laborum nesciunt ipsum dolore error repellendus officiis
+                  aliquid a, vitae reprehenderit, accusantium vero, ad.
+                  Obcaecati numquam sapiente cupiditate. Praesentium eaque, quae
+                  error!
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Perferendis, maiores.
+                </p>
               </div>
-
-              <div className="layout-row layout-align-end">
-                <button className="button-primary" onClick={createNewPosting}>
-                  Create
-                </button>
-              </div>
-            </div>
-            <div className="tab2">
-              <h2>Second</h2>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Laborum nesciunt ipsum dolore error repellendus officiis aliquid
-                a, vitae reprehenderit, accusantium vero, ad. Obcaecati numquam
-                sapiente cupiditate. Praesentium eaque, quae error!
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Perferendis, maiores.
-              </p>
-            </div>
-            <div className="tab3">
-              <h2>Third</h2>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio,
-                nobis culpa rem, vitae earum aliquid.
-              </p>
-            </div>
+            )}
           </section>
         </div>
       </div>
